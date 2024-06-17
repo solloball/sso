59dc670be58ebdd47d18bb73338742e790354a5e
solloball <romanov.k48@gmail.com>
2024-06-17 12:32:07 +0700

    init logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
-    "fmt"
+    "os"
+    "log/slog"
 
     "github.com/solloball/sso/internal/config"
+)
+
+const (
+    envLocal = "local"
+    envDev = "dev"
+    envProd = "prod"
 )
 
 func main() {
     cfg := config.MustLoad()
 
-    //TODO:: remove it
-    fmt.Println(cfg)
 
+    log := setupLogger(cfg.Env)
+
+    log.Info("starting application", slog.String("env", cfg.Env))
     //TODO:: init logger
 
     //TODO:: init app
 
     //TODO:: run grpc service
+}
+
+func setupLogger(env string) *slog.Logger {
+    var log *slog.Logger
+
+    switch env {
+    case envLocal:
+        log = slog.New(
+            slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envDev:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envProd:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError}),
+        )
+    }
+
+    return log
 }
